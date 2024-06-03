9ff10fbe221a4e7d06c995e69c02f78ad4a87e97
Wenjicai <96035617+Wenjicai@users.noreply.github.com>
2024-06-03 16:52:57 +0800

    Update AdminController.java
    增加了注释

--- Code/Backend/src/main/java/com/example/springboot/controller/AdminController.java
+++ Code/Backend/src/main/java/com/example/springboot/controller/AdminController.java
@@ -17,17 +17,18 @@ public class AdminController {
     String uid = new UID().produceUID();
 
     @Resource
-    private AdminService adminService;
+    private AdminService adminService; //注入管理员服务，用于调用相关业务逻辑
 
     /**
      * 管理员登录
      */
     @PostMapping("/login")
     public Result<?> login(@RequestBody User user, HttpSession session) {
+        //调用服务层方法验证用户
         Object o = adminService.adminLogin(user.getUsername(), user.getPassword());
         if (o != null) {
             System.out.println(o);
-            //存入session
+            //验证成功，将用户信息存入session
             session.setAttribute("Identity", "admin");
             session.setAttribute("User", o);
             return Result.success(o);
@@ -41,6 +42,7 @@ public class AdminController {
      */
     @PutMapping("/update")
     public Result<?> update(@RequestBody Admin admin) {
+        //调用服务处方法更新信息
         int i = adminService.updateAdmin(admin);
         if (i == 1) {
             return Result.success();
